2b044d52fd2bae0cbd2e20a9de943db322e868bc
Caleb Wang <cw@berkeley.edu>
2012-12-27 10:41:53 -0800

    small changes

--- app.js
+++ app.js
@@ -12,24 +12,29 @@ app.get('/', function(req, res){
     res.render('about', {
                          title: 'Caleb'
                         })
+    console.log('Serving /');
 });
 
 app.get('/about', function(req, res){
     res.render('about', {
                          title: 'Caleb'
                         })
+    console.log('Serving /about');
 });
 
 app.get('/projects', function(req, res){
     res.render('projects', {title: 'Projects'});
+    console.log('Serving /projects');
 });
 
 app.get('/contact', function(req, res){
     res.render('contact', {title: 'Contact'});
+    console.log('Serving /contact');
 });
 
 app.get('/blog', function(req, res){
     res.render('blog', {title: 'Blog'});
+    console.log('Serving /blog');
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8000);
