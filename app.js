bccf99a00c2cc37325bdc3493bff94e6bc427705
Caleb Wang <cw@berkeley.edu>
2012-12-29 12:24:40 -0800

    testing a resume/project template. learning js.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var poet = require('poet')( app );
+var fs = require('fs')
 
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
@@ -46,7 +47,41 @@ app.get('/blog', function(req, res){
     console.log('Serving /blog');
 });
 
+function myCallback(data){
+    return data;
+}
+
+function readContent(filename, callback) {
+    fs.readFile(__dirname + '/public' + filename, function(err, data) {
+        if (err) return callback(err)
+        callback(data)
+    })
+}
+
+app.get('/test', function(req, res){
+    fs.readFile('./public/files/test.json', function(err, data){
+        if (err) throw err;
+        console.log(JSON.parse(data)); 
+        return JSON.parse(data);
+    });
+
+    res.render('test', {
+                        title: 'test',
+                        getJSON: function(filename){
+                            return JSON.parse(fs.readFileSync(__dirname + '/public' + filename))
+                            },
+                        listFiles: function(){
+                                var myArr = Array()
+                                myArr[0] = '/files/test.json';
+                                return myArr
+                            }
+                        }
+    );    
+
+    console.log('Serving /test')
+});
 var port = process.env.PORT || 8000
+
 app.listen(port, function() {
   console.log('Listening on port ' + port);
 });
